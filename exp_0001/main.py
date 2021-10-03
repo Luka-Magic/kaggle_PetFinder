ad5996e9b00545858c7f00a0a8234efcb8644d6c
Luka-Magic <stargazinger0928@gmail.com>
2021-10-03 22:36:34 +0900

    update

--- exp_0001/main.py
+++ exp_0001/main.py
@@ -244,7 +244,7 @@ def valid_one_epoch(epoch, model, loss_fn, data_loader, device):
     return score_epoch, loss.detach().cpu().numpy()
 
 
-@hydra.main(config_path='config.yaml')
+@hydra.main(config_name='config')
 def main(cfg: DictConfig):
     seed_everything(cfg.seed)
 
