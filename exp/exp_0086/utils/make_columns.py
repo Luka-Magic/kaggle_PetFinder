a4078bd5d7b5be3c69afd046a77e49abe39adab4
Luka-Magic <stargazinger0928@gmail.com>
2021-10-23 16:09:09 +0900

    update

---
+++ exp/exp_0086/utils/make_columns.py
@@ -0,0 +1,11 @@
+def make_columns(columns):
+    return_columns = []
+    if 'original' in columns or 'all' in columns:
+        return_columns.extend(['Subject Focus', 'Eyes', 'Face', 'Near', 'Action',
+                                'Accessory', 'Group', 'Collage', 'Human', 'Occlusion', 'Info', 'Blur'])
+    if 'basic' in columns or 'all' in columns:
+        return_columns.extend(
+            ['height', 'width', 'size', 'sqrtsize', 'aspect'])
+    if 'hash' in columns or 'all' in columns:
+        return_columns.extend([f'hash_{i}' for i in range(256)])
+    return return_columns
