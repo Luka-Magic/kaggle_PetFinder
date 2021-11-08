b3c36479f0b0842a3df0df3c6b83ef0a3e6e434c
Luka-Magic <stargazinger0928@gmail.com>
2021-11-09 04:10:49 +0900

    update

--- exp/exp_0097/main.py
+++ exp/exp_0097/main.py
@@ -85,7 +85,7 @@ class pf_dataset(Dataset):
 
         h, w, _ = img_rgb.shape
         h_pad, w_pad = max((w - h)//2, 0), max((h - w)//2, 0)
-        if self.phase == 'valid':
+        if self.phase == 'valid' or 'train':
             h_dis, w_dis = max((h - w)//2, 0), max((w - h)//2, 0)
             img_rgb = img_rgb[h_dis:h-h_dis, w_dis:w-w_dis, :]
 
