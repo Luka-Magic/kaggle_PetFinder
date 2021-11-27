bb6a942366b9dba404a5223d5ba40a79e55c9d1d
Luka-Magic <stargazinger0928@gmail.com>
2021-11-27 17:48:40 +0900

    update

--- exp/exp_0128/main.py
+++ exp/exp_0128/main.py
@@ -204,7 +204,10 @@ class pf_multiloss(nn.Module):
 
     def forward(self, inputs, target):
         labels = self.get_labels(target)
-        # print(labels)
+        print(inputs)
+        print(inputs[0].shape)
+        print(labels[0].shape)
+        
         loss = [weight * criterion(input, target) for criterion, weight, input,
                 target in zip(self.loss_fns, self.weights, inputs, labels)]
         return sum(loss)
