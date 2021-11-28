0a95f707f3c1bfde82ed3c428ee687fa930207ac
Luka-Magic <stargazinger0928@gmail.com>
2021-11-28 10:28:53 +0900

    update

--- EDA/maketrain_0003.py
+++ EDA/maketrain_0003.py
@@ -21,6 +21,7 @@ def img_hash(series):
     image = Image.open(series.file)
     return np.array([f(image).hash for f in funcs]).reshape(256).astype(np.uint8)
 
+
 def make_hash_df(file_path_ser: pd.Series):
     '''
     file_path => hash_df
@@ -41,3 +42,32 @@ def delete_sim_img(hash_df, theshold=0.8):
     hash_df.loc[indices1, 'sims'] = 1
     return hash_df
 
+
+def preprocess(data_path, phase):
+    '''
+    data_path: path of data
+    phase: train/test
+    '''
+    df = pd.read_csv(os.path.join(data_path, f'{phase}.csv'))
+
+    df['file_path'] = df['Id'].apply(
+        lambda x: os.path.join(data_path, f'{phase}/{x}.jpg'))
+
+    hash_df = make_hash_df(df['file_path'])
+
+    hash_df = delete_sim_img(hash_df)
+
+    return hash_df
+
+
+def load_to_csv(num):
+    # この関数はここでしか使わない
+    data_path = '/content/drive/MyDrive/kaggle_PetFinder/data'
+    processed_df = preprocess(
+        data_path, 'train')
+    processed_df.to_csv(os.path.join(data_path, f'train_{num}.csv'))
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    load_to_csv(args[0])
