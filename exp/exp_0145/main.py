f7c41fa920bd8942d40f091650bedd56a34df998
Luka-Magic <stargazinger0928@gmail.com>
2021-12-08 02:59:54 +0900

    update

--- exp/exp_0145/main.py
+++ exp/exp_0145/main.py
@@ -322,9 +322,8 @@ def train_valid_one_epoch(cfg, epoch, model, loss_fn, optimizer, train_loader, v
 
     if epoch == 0:
         for name, param in model.named_parameters():
-            print(name)
-            # if re.search('model', name):
-            #     param.requires_grad = False
+            if re.search('model', name):
+                param.requires_grad = False
 
     pbar = tqdm(enumerate(train_loader), total=len(train_loader))
 
