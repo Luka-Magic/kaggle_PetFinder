035021aeda6eba53a6a2d3dbb132e67aab2b1774
Luka-Magic <stargazinger0928@gmail.com>
2021-12-08 01:54:51 +0900

    update

--- exp/exp_0144/main.py
+++ exp/exp_0144/main.py
@@ -270,6 +270,7 @@ def get_preds(cfg, preds):
             interval = 100 // cls
             outputs += [np.sum((torch.sigmoid(pred).detach().cpu().numpy()
                                 * interval), axis=1)][:, np.newaxis]
+    print([output.shape for output in outputs])
     return np.mean(np.concatenate(outputs), axis=1)[:, np.newaxis]
 
 
