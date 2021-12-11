7c0cea97794beb6c8006d09ab4c8bafa8641f29d
Luka-Magic <stargazinger0928@gmail.com>
2021-12-11 22:48:50 +0900

    update

--- exp/exp_0152/main.py
+++ exp/exp_0152/main.py
@@ -233,14 +233,14 @@ class RegLoss(nn.Module):
     def forward(self, input, target):
         losses = []
         target_reg = target.float().view(-1, 1)
-        if self.loss == 'BCEWithLogitsLoss' or self.loss == 'FOCALLoss':
-            target_reg /= 100
         for cls, pred in zip(self.cls, input):
             pred = self.calc_pred(cls, pred)
+
             if self.loss == 'BCEWithLogitsLoss' or self.loss == 'FOCALLoss':
+                target_reg /= 100
                 pred /= 100
             losses.append(self.reg_criterion(
-                pred, target))
+                pred, target_reg))
         return sum(losses) / len(losses)
 
     def calc_pred(self, cls, pred):
