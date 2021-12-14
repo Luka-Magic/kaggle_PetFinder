1e86a597715d422a15563d36a4d3b1277e5d17d6
Luka-Magic <stargazinger0928@gmail.com>
2021-12-15 02:02:51 +0900

    update

--- exp/exp_0160/main.py
+++ exp/exp_0160/main.py
@@ -331,6 +331,9 @@ def train_valid_one_epoch(cfg, epoch, model, loss_fn, optimizer, train_loader, v
                 else:
                     print(
                         f"train: {train_score:.5f}, valid: {valid_score:.5f}, epoch: {epoch}, step: {step}")
+            # early stopping
+            if cfg.mix_p == 0 and train_score - valid_score < -2.:
+                return None
 
     if cfg.mix_p == 0:
         preds_epoch = np.concatenate(preds_all)
