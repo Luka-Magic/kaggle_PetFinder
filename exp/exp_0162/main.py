5c51a3713e6a9b4a3afab94e0bf24d4d0c9c75ab
Luka-Magic <stargazinger0928@gmail.com>
2021-12-15 04:19:13 +0900

    update

--- exp/exp_0162/main.py
+++ exp/exp_0162/main.py
@@ -331,6 +331,9 @@ def train_valid_one_epoch(cfg, epoch, model, loss_fn, optimizer, train_loader, v
                 else:
                     print(
                         f"train: {train_score:.5f}, valid: {valid_score:.5f}, epoch: {epoch}, step: {step}")
+            # early stopping
+            if cfg.mix_p == 0 and train_score - valid_score < -2.:
+                return 'stop'
 
     if cfg.mix_p == 0:
         preds_epoch = np.concatenate(preds_all)
@@ -340,6 +343,7 @@ def train_valid_one_epoch(cfg, epoch, model, loss_fn, optimizer, train_loader, v
         print(f'TRAIN: {train_score:.5f}, VALID: {valid_score:.5f}')
     else:
         print(f'VALID: {valid_score:.5f}')
+    return 'finish'
 
 
 def preprocess(cfg, train_fold_df, valid_fold_df):
@@ -476,8 +480,10 @@ def main(cfg: DictConfig):
 
         for epoch in tqdm(range(cfg.epoch), total=cfg.epoch):
             # Train Start
-            train_valid_one_epoch(cfg, epoch, model, loss_fn, optim, train_loader,
+            result = train_valid_one_epoch(cfg, epoch, model, loss_fn, optim, train_loader,
                                   valid_loader, device, scheduler, scaler, best_score, model_name)
+            if result == 'stop':
+                break
 
         print('=' * 40)
         print(
