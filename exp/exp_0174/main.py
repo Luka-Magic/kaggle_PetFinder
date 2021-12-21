a7273961dfcb7ea0e9b670be179d6e930d6f3d75
Luka-Magic <stargazinger0928@gmail.com>
2021-12-21 15:13:10 +0900

    update

--- exp/exp_0174/main.py
+++ exp/exp_0174/main.py
@@ -277,6 +277,9 @@ def train_valid_one_epoch(cfg, epoch, model, loss_fn, optimizer, train_loader, v
         targets = targets.to(device).float()
         labels = labels.to(device).long()
 
+        print(targets.shape)
+        print(labels.shape)
+
         with autocast():
             mix_p = np.random.rand()
             mix_list = list(range(cfg.init_nomix_epoch,
@@ -288,6 +291,7 @@ def train_valid_one_epoch(cfg, epoch, model, loss_fn, optimizer, train_loader, v
                     preds, labels[0]) * labels[2] + loss_fn(preds, labels[1]) * (1. - labels[2])
             else:
                 preds = model(imgs)
+                print(preds.shape)
                 loss = loss_fn(preds, labels)
         losses.update(loss.item(), cfg.train_bs)
         scaler.scale(loss).backward()
